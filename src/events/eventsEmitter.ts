52efeadb39996c9eccf92d285f31f4f8c4d98b04
Adasf <osjegaar@gmail.com>
2024-07-23 01:29:54 -0600

    Added EventEmitter class for handling animation events, improving event management and extensibility

---
+++ src/events/eventsEmitter.ts
@@ -0,0 +1,30 @@
+import { Listener, NumericalAnimatorEvents } from "../types/types";
+
+export default class EventEmitter {
+  private readonly events: Map<NumericalAnimatorEvents, Listener[]> = new Map();
+
+  public on<T>(
+    eventType: NumericalAnimatorEvents,
+    listener: Listener<T> | undefined
+  ) {
+    if (typeof listener !== "function") {
+      return;
+    }
+
+    if (!this.events.has(eventType)) {
+      this.events.set(eventType, [listener]);
+    } else {
+      this.events.get(eventType)!.push(listener);
+    }
+  }
+
+  public emit<T>(eventType: NumericalAnimatorEvents, ...args: T[]) {
+    if (!this.events.has(eventType)) {
+      return;
+    }
+
+    this.events.get(eventType)!.forEach((listener) => {
+      (listener as Listener<T>)(...args);
+    });
+  }
+}
