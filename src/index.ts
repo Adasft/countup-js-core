1f9057887851552d0babf7f4ff4bf58ad1275ed7
Adasf <osjegaar@gmail.com>
2024-07-23 01:30:45 -0600

    export names were changed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import NumericalAnimatorCore from "./core/numericalAnimatorCore";
-import RequestAnimationFrame from "./core/requestAnimationFrame";
 import UtilityEasingFunctions from "./utils/easingFunctions";
 
-export { NumericalAnimatorCore, RequestAnimationFrame, UtilityEasingFunctions };
+export { NumericalAnimatorCore, UtilityEasingFunctions };
