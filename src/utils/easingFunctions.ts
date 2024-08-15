22e90b43f76f218e453bf0fe235fadf064a56999
Adasf <osjegaar@gmail.com>
2024-08-15 01:39:21 -0600

    chore: Update utility easing functions and animator in demo

--- src/utils/easingFunctions.ts
+++ src/utils/easingFunctions.ts
@@ -1,18 +1,18 @@
-import { EasingFunction, EasingUtil } from "../types/types";
+import { EasingFunction, EasingUtil } from "../types";
 
-const UtilityEasingFunctions: EasingUtil = Object.freeze({
-  linear: (t) => t,
-  easeOutCubic: (t) => 1 - Math.pow(1 - t, 3),
-  easeInQuad: (t) => t * t,
-  easeOutQuad: (t) => t * (2 - t),
-  easeInOutQuad: (t) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
-  easeOutSine: (t) => Math.sin((t * Math.PI) / 2),
-  easeInOutSine: (t) => -(Math.cos(Math.PI * t) - 1) / 2,
-  easeInOutQuart: (t) =>
+const UtilityEasingFunctions: EasingUtil = Object.freeze<EasingUtil>({
+  linear: (t: number) => t,
+  easeOutCubic: (t: number) => 1 - Math.pow(1 - t, 3),
+  easeInQuad: (t: number) => t * t,
+  easeOutQuad: (t: number) => t * (2 - t),
+  easeInOutQuad: (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+  easeOutSine: (t: number) => Math.sin((t * Math.PI) / 2),
+  easeInOutSine: (t: number) => -(Math.cos(Math.PI * t) - 1) / 2,
+  easeInOutQuart: (t: number) =>
     t < 0.5 ? 8 * t * t * t * t : 1 - Math.pow(-2 * t + 2, 4) / 2,
-  easeInOutQuint: (t) =>
+  easeInOutQuint: (t: number) =>
     t < 0.5 ? 16 * t * t * t * t * t : 1 - Math.pow(-2 * t + 2, 5) / 2,
-  easeOutExpo: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+  easeOutExpo: (t: number) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
 });
 
 export function getEasingFunctionByName(
