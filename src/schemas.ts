b037ce11622d70834cc2c3e8bad3bb3d9f700819
Adasf <osjegaar@gmail.com>
2024-08-15 01:33:09 -0600

    Refactor: Consolidate type definitions and add schemas for options validation

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,49 +1,74 @@
-import TypeChecker, { InferType } from "./utils/typeChecking";
+import { Type } from "./utils/typeChecking";
 
-export const optionsSchema = TypeChecker.object({
-  start: TypeChecker.number(true),
-  end: TypeChecker.number(true),
-  duration: TypeChecker.number(true),
-  easingFunction: TypeChecker.oneOfType([
-    TypeChecker.string(),
-    TypeChecker.func(),
-  ]),
-  decimalPlaces: TypeChecker.number(),
-  autoPlay: TypeChecker.boolean(),
-  middleware: TypeChecker.array([
-    TypeChecker.func(
-      [TypeChecker.number(), TypeChecker.string()],
-      TypeChecker.number(),
-      true
-    ),
-  ]),
-  onChange: TypeChecker.func(),
-  onComplete: TypeChecker.func(),
-  onPlay: TypeChecker.func(),
-  onPause: TypeChecker.func(),
-  onReset: TypeChecker.func(),
-  onUpdate: TypeChecker.func(),
-  onStop: TypeChecker.func(),
-});
+export namespace Schemas {
+  export const middlewareSchema = Type.func(
+    [Type.oneOfType([Type.number(true), Type.string(true)])],
+    Type.oneOfType([Type.number(true), Type.string(true)]),
+    true
+  );
 
-const fnSchema = TypeChecker.func(
-  [TypeChecker.number(true), TypeChecker.string(true)],
-  TypeChecker.number()
-);
+  export const easingFunctionNameSchema = Type.enumeration([
+    "linear",
+    "easeOutCubic",
+    "easeInQuad",
+    "easeOutQuad",
+    "easeInOutQuad",
+    "easeOutSine",
+    "easeInOutSine",
+    "easeInOutQuart",
+    "easeInOutQuint",
+    "easeOutExpo",
+  ]);
 
-type fnT = InferType<typeof fnSchema>;
+  export const easingFunctionHandlerSchema = Type.func(
+    [Type.number(true)],
+    Type.number(true),
+    true
+  );
 
-export const updatedOptionsSchema = TypeChecker.object({
-  end: TypeChecker.number(),
-  duration: TypeChecker.number(),
-  decimalPlaces: TypeChecker.number(),
-  easingFunction: TypeChecker.oneOfType([
-    TypeChecker.string(),
-    TypeChecker.func(),
-  ]),
-});
+  export const easingFunctionSchema = Type.oneOfType([
+    easingFunctionNameSchema,
+    easingFunctionHandlerSchema,
+  ]);
 
-export const modifiedValueSchema = TypeChecker.oneOfType([
-  TypeChecker.number(),
-  TypeChecker.string(),
-]);
+  export const optionsSchema = Type.object(
+    {
+      start: Type.number(true),
+      end: Type.number(true),
+      duration: Type.number(true),
+      abortOnError: Type.boolean(),
+      debug: Type.boolean(),
+      easingFunction: Type.optional(easingFunctionSchema),
+      decimalPlaces: Type.number(),
+      autoPlay: Type.boolean(),
+      middleware: Type.array([middlewareSchema]),
+      onChange: Type.func(
+        [Type.oneOfType([Type.number(true), Type.string(true)])],
+        undefined
+      ),
+      onComplete: Type.func([], undefined),
+      onPlay: Type.func([], undefined),
+      onPause: Type.func([], undefined),
+      onReset: Type.func([], undefined),
+      onUpdate: Type.func([], undefined),
+      onStop: Type.func([], undefined),
+      onError: Type.func([Type.string(true)], undefined),
+    },
+    true
+  );
+
+  export const updatedOptionsSchema = Type.object(
+    {
+      end: Type.number(),
+      duration: Type.number(),
+      decimalPlaces: Type.number(),
+      easingFunction: Type.optional(easingFunctionHandlerSchema),
+    },
+    true
+  );
+
+  export const modifiedValueSchema = Type.oneOfType([
+    Type.number(true),
+    Type.string(true),
+  ]);
+}
